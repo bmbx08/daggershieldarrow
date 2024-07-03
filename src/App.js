c436da5bad178d9974422898a1e2a96ab049bcd9
bmbx08 <bmbx08@bu.ac.kr>
2024-07-04 00:17:32 +0900

    added css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import {useState} from "react"
+import {useState} from "react";
 import "./App.css";
 import Box from "./component/Box";
+import versus from "./images/vs.png";
+
+// import dagger from './images/dagger1.png'
+// import bow from './images/bow1.png'
+// import staff from './images/staff1.png'
 
 // 1. 박스 2개(타이틀, 사진, 결과)
 // 2. 가위,바위,보 버튼이 있다
@@ -23,53 +28,114 @@ const choice = {
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1200px-Rock-paper-scissors_%28scissors%29.png",
   },
 };
+
+// dagger
+// https://image.similarpng.com/very-thumbnail/2021/06/Hand-drawn-Dagger-on-transparent-PNG.png
+
+// bow and arrow
+// bow and arrow png site: https://www.pngall.com/bow-arrow-png/
+// https://www.pngall.com/wp-content/uploads/15/Bow-Arrow-PNG-Photos.png
+
+// mage staff
+// https://www.vhv.rs/dpng/d/504-5043226_wizard-staff-png-transparent-png.png
+
 function App() {
-  const[userSelect, setUserSelect]=useState(null);
-  const[computerSelect, setComputerSelect]=useState(null);
-  const[result,setResult]=useState("");
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [userResult, setUserResult] = useState("");
+  const [compResult, setCompResult] = useState("");
+  const [userScore, setScore] = useState(0);
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
-    setResult(judgement(choice[userChoice],computerChoice));
+    let userResult = userJudgement(choice[userChoice], computerChoice);
+    setUserResult(userResult);
+    setCompResult(compJudgement(userResult));
+    calcScore(userResult);
+  };
+
+  const calcScore = (result) => {
+    if((userScore <= 1) &&(result==="Lose")){
+      setScore(0);
+    }
+    else if (result !== "Tie") {
+      setScore(result === "Win" ? (userScore+3) : (userScore-2));
+    }
+    
   };
 
   const randomChoice = () => {
-    let itemArray = Object.keys(choice);//Object.keys(객체)->객체의 키값만 뽑아서 array로 만들어주는 함수
-    let randomNum = Math.floor(Math.random()*itemArray.length);
+    let itemArray = Object.keys(choice); //Object.keys(객체)->객체의 키값만 뽑아서 array로 만들어주는 함수
+    let randomNum = Math.floor(Math.random() * itemArray.length);
     let final = itemArray[randomNum];
     return choice[final];
-  }
+  };
 
-  const judgement = (user,computer) => {
-    const userChoice=user.name;
-    const compChoice=computer.name;
-    console.log("user",userChoice,"computer",compChoice);
-    if (userChoice===compChoice){
-      return "Tie"
-    } else if(userChoice==="Rock"){
-      return(compChoice === "Scissors"?"Win":"Lose")
-    } else if(userChoice==="Paper"){
-      return(compChoice === "Rock"?"Win":"Lose")
-    } else if(userChoice==="Scissors"){
-      return(compChoice === "Paper"?"Win":"Lose")
-    } else{
+  const userJudgement = (user, computer) => {
+    const userChoice = user.name;
+    const compChoice = computer.name;
+    console.log("user", userChoice, "computer", compChoice);
+    if (userChoice === compChoice) {
+      return "Tie";
+    } else if (userChoice === "Rock") {
+      return compChoice === "Scissors" ? "Win" : "Lose";
+    } else if (userChoice === "Paper") {
+      return compChoice === "Rock" ? "Win" : "Lose";
+    } else if (userChoice === "Scissors") {
+      return compChoice === "Paper" ? "Win" : "Lose";
+    } else {
       return "error";
     }
-  }
+  };
+
+  const compJudgement = (user) => {
+    if (user === "Tie") {
+      return "Tie";
+    } else if (user !== "Tie") {
+      return user === "Win" ? "Lose" : "Win";
+    } else return "Error";
+  };
 
   return (
     <div>
-      <div className="main main-section">
-        <Box title="You" item={userSelect} result={result}/>
-        <Box title="Computer" item={computerSelect} result={result}/>
+      <div className="top-section">
+        <div></div>
+        <div className="title-box">
+          <h1 className="title">Rock Paper Shoo!</h1>
+        </div>
+        <div className="score-box">
+          <div className="score-section">
+            <h3>Total Score: </h3>
+            <h3>{userScore}</h3>
+          </div>
+        </div>
+        
       </div>
 
-      <div className="main button-section">
-        <button onClick={() => play("scissors")}>가위</button> {/*리액트에서는 onClick 안에 함수로, 매개변수 있을 시 콜백형태 함수를 적어야됨*/}
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+      <div className="middle-section">
+        <div className="main-section">
+          <Box title="You" item={userSelect} result={userResult} />
+          <div className="versus-section">
+            <img src={versus} className="versus-img" />
+          </div>
+          <Box title="Computer" item={computerSelect} result={compResult} />
+          {/* <img src={dagger}/> */}
+        </div>
+      </div>
+
+      <div className="button-section">
+        <button onClick={() => play("scissors")}>
+          <i class="fa-regular fa-hand-scissors icon"></i>
+        </button>{" "}
+        {/*리액트에서는 onClick 안에 함수로, 매개변수 있을 시 콜백형태 함수를 적어야됨*/}
+        <button onClick={() => play("rock")}>
+          <i class="fa-regular fa-hand-back-fist icon"></i>
+        </button>
+        <button onClick={() => play("paper")}>
+          <i class="fa-regular fa-hand icon"></i>
+        </button>
       </div>
       {/* <Box user="you" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjwLvDG1Zgl3hsxUzfSx-xmAPOjw2BpJ9N5Q&s"/>
       <Box user="computer" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png"/> */}
@@ -77,12 +143,10 @@ function App() {
   );
 }
 
-
 export default App;
 
-
-
-{/*아이디어
+{
+  /*아이디어
   1. 단검, 칼방패, 화살로 변경(버튼마다 소리 추가)
   2. 총 점수 표시, 이기면 +3, 지면 -2
   3. 우측하단에 컴퓨터 확률 알려주기(심리)
@@ -102,4 +166,5 @@ export default App;
 
   *잘못 누르면 패턴 바뀜, 시간 안에 달성해야됨, 6개 맞추면 10점 추가, 실패시 -5점
   
-  */}
+  */
+}
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 const Box = (props) => {
-  let result;
-  if (
-    props.title === "Computer" &&
-    props.result !== "Tie" &&
-    props.result !== ""
-  ){
-    result = props.result === "Win" ? "Lose" : "Win";
-  }else {
-    result = props.result;
-  }
+  // let result;
+  // if (
+  //   props.title === "Computer" &&
+  //   props.result !== "Tie" &&
+  //   props.result !== ""
+  // ){
+  //   result = props.result === "Win" ? "Lose" : "Win";
+  // }else {
+  //   result = props.result;
+  // }
 
   return (
     <div className="box">
-      <h1>{props.title}</h1>
+      <h1 className="box-title">{props.title}</h1>
       <img className="item-img" src={props.item && props.item.img} />
       {/*'props.item&&'는 에러 방지용으로 넣는다.*/}
-      <h2 className={`${result}`}>{result}</h2>
+      <h2 className={`${props.result} result`}>{props.result}</h2>
     </div>
   );
 };
